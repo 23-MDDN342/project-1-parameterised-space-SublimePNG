81d17499f98d5a9797c6eed9c103f323f3d97de2
SublimePNG <80139430+SublimePNG@users.noreply.github.com>
2023-03-28 11:21:21 +1300

    basic pixel movement

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -3,7 +3,7 @@ var y=300;
 var a=100;
 var b=100;
 
-function draw_one_frame() {
+function draw_one_frame(cur_frac) {
   background(255);
 //////////////////////////////////////////////////
   //drawHexagon(100, 100, 70);
@@ -20,8 +20,8 @@ function drawHexagon(x, y, radius) {
 ////////////////////////////////////////////////////
 let primary = color("#202020")
 let secondary = color ("#FF0059")
-let hexColour = color("#202020")
 
+noStroke();
 //////////////////////////////////////////////////////
 let noiseColour = getNoiseValue (0,0, 0.8, "noiseColour",0 ,1, 1)
 let noisyColour;
@@ -31,12 +31,25 @@ let pixelSpacing =width / 10
 
 for(let x = 0; x < width / pixelSpacing; x++){
     for(let y = 0; y < height / pixelSpacing; y++){
-	noiseColour = getNoiseValue (pixelSpacing*x,pixelSpacing*y, 0.8, "colour",0 ,1, 1)
-	noisyColour = lerpColor(primary,secondary,noiseColour)
-	fill(noisyColour)
-	rect(pixelSpacing*x,pixelSpacing*y, pixelSize);
+	
+      noiseColour = getNoiseValue (pixelSpacing*x,pixelSpacing*y, 0.8, "colour",0 ,1, 1)
+      noisyColour = lerpColor(primary,secondary,noiseColour)
+	
+  
+    moveXMap = map(cur_frac, 0, 1, pixelSpacing*x, pixelSize*(x+1))
+    fill(noisyColour)
+    rect(moveXMap,pixelSpacing*y, pixelSize) 
+  
+
+ 
+   
+
+
+  
+	//rect(pixelSpacing*x,pixelSpacing*y, pixelSize)
 }
 }
+
 
 
 
@@ -47,10 +60,13 @@ for(let x = 0; x < width / pixelSpacing; x++){
 
 ///////////////////////////////////////////////////////
 let hexSize = 70
+let hexSizeSmall = 64
 let x_spacing = 220
 let y_spacing = 64
 
-fill(hexColour)
+let hexColour = color("#202020")
+let hexColourSmall = color("#171717")
+
 
 
 for(let x = 0; x < width / x_spacing; x++){
@@ -62,7 +78,13 @@ for(let x = 0; x < width / x_spacing; x++){
       if (y % 2 == 1) { 
         xPos += x_spacing / 2;
       }
+      fill(hexColour)
+      stroke(0)
       drawHexagon(xPos, yPos, hexSize);
+      
+      fill(hexColourSmall)
+      noStroke()
+      drawHexagon(xPos, yPos, hexSizeSmall);
     }
   }
 }
