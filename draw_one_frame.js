db3cb600ae7776fe4f19ea01a907845c94af9138
SublimePNG <80139430+SublimePNG@users.noreply.github.com>
2023-03-30 19:32:38 +1300

    Fix?

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -18,41 +18,85 @@ function drawHexagon(x, y, radius) {
   endShape(CLOSE);
 }
 ////////////////////////////////////////////////////
-//let primary = colour(afafa)
-//let secondary = colour (sawg)
+<<<<<<< HEAD
+let primaryLight = color("#00f2ff")
+let secondaryLight = color ("#FF0059")
 let hexColour = color("#202020")
+let hexColourSmall = color("#131313")
+=======
+let primary = color("#202020")
+let secondary = color ("#FF0059")
+>>>>>>> 81d17499f98d5a9797c6eed9c103f323f3d97de2
+
+noStroke();
+//////////////////////////////////////////////////////
+let noiseColour = getNoiseValue (0,0, 0.8, "noiseColour",0 ,1, 1)
+let noisyColour;
+<<<<<<< HEAD
+let pixelSize =width / 10
+let pixelSpacing =width / 50
+
+for(let x = 0; x < width / pixelSpacing; x++){
+    for(let y = 0; y < height / pixelSpacing; y++){
+=======
+
+let pixelSize =width / 10
+let pixelSpacing =width / 10
+
+for(let x = 0; x < width / pixelSpacing; x++){
+    for(let y = 0; y < height / pixelSpacing; y++){
+  
+      noiseColour = getNoiseValue (pixelSpacing*x,pixelSpacing*y, 0.8, "colour",0 ,1, 1)
+      noisyColour = lerpColor(primary,secondary,noiseColour)
+  
+  
+    moveXMap = map(cur_frac, 0, 1, pixelSpacing*x, pixelSize*(x+1))
+    fill(noisyColour)
+    rect(moveXMap,pixelSpacing*y, pixelSize) 
+  
+
+ 
+   
+
+  
+  //rect(pixelSpacing*x,pixelSpacing*y, pixelSize)
+}
+}
 
 
-//let noiseColour = getNoiseValue (0,0, 0.8, "colour",0 ,1, 1)
-//let noisyColour;
-
-//let pixelSize =width / 1
-//let pixelSpacing =width / 100
 
 
-//for(let x = 0; x < width / pixelSpacing; x++){
-    //for(let y = 0; y < height / pixelSpacing; y++){
-	//noiseColour = getNoiseValue (pixelSpacing*x,pixelSpacing*y, 0.8, "colour",0 ,1, 1)
-	//noisyColour = lerpColour(primary,secondary)
-	//fill(noisyColour)
-	//rect(pixelSpacing*x,pixelSpacing*y, pixelSize);
-//}
-//}
+>>>>>>> 81d17499f98d5a9797c6eed9c103f323f3d97de2
 
+  noiseColour = getNoiseValue (pixelSpacing*x,pixelSpacing*y, cur_frac/ 2, "colour",0 ,1, 200)
+  noisyColour = lerpColor(primaryLight,secondaryLight,noiseColour)
+  fill(noisyColour)
+  rect(pixelSpacing*x,pixelSpacing*y, pixelSize);
 
-
-
-
-
-
+    moveXMap = map(cur_frac, 0, 1, pixelSpacing*x, pixelSize*(x+1))
+    fill(noisyColour)
+    rect(moveXMap,pixelSpacing*y, pixelSize) 
+}
+}
 
 ///////////////////////////////////////////////////////
 // hexgrid settings
 let hexSize = 70
+<<<<<<< HEAD
+let hexSizeSmall = 62
 let x_spacing = 220
 let y_spacing = 64
 
-fill(hexColour)
+//fill(hexColour)
+//let hexColour = color("#202020")
+=======
+let hexSizeSmall = 64
+let x_spacing = 220
+let y_spacing = 64
+
+let hexColour = color("#202020")
+let hexColourSmall = color("#171717")
+>>>>>>> 81d17499f98d5a9797c6eed9c103f323f3d97de2
 
 
 // draw a hexagonal grid
@@ -60,13 +104,22 @@ for(let x = 0; x < width / x_spacing; x++){
     for(let y = 0; y -1 < height / y_spacing; y++){
       let xPos = x_spacing * x;
       let yPos = y_spacing * y;
-	  // add an offset to every second row
+    // add an offset to every second row
       if (y % 2 == 1) { 
         xPos += x_spacing / 2;
       }
       fill(hexColour)
       stroke(0)
       drawHexagon(xPos, yPos, hexSize);
+<<<<<<< HEAD
+
+=======
+      
+>>>>>>> 81d17499f98d5a9797c6eed9c103f323f3d97de2
+      fill(hexColourSmall)
+      noStroke()
+      drawHexagon(xPos, yPos, hexSizeSmall);
     }
   }
 }
+
